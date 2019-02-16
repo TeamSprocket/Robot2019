c596189b800d3b615f750acf88a7f1553fd822d0
alfphaderp <36178918+alfphaderp@users.noreply.github.com>
2019-02-16 14:14:53 -0800

    Create ChickenPotPie

---
+++ src/main/java/frc/util/drivers/ChickenPotPie.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.util.drivers;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
+
+/**
+ * TODO: An AnalogPotentiometer 
+ */
+public class ChickenPotPie extends AnalogPotentiometer {
+  public ChickenPotPie(AnalogInput input) {
+    super(input);
+  }
+
+  public ChickenPotPie(AnalogInput input, double scale) {
+    super(input, scale);
+  }
+
+  public ChickenPotPie(AnalogInput input, double fullRange, double offset) {
+    super(input, fullRange, offset);
+  }
+
+  public ChickenPotPie(int channel) {
+    this(new AnalogInput(channel));
+  }
+
+  public ChickenPotPie(int channel, double scale) {
+    this(new AnalogInput(channel), scale);
+  }
+
+  public ChickenPotPie(int channel, double fullRange, double offset) {
+    this(new AnalogInput(channel), fullRange, offset);
+  }
+
+  // public double getRate() {
+  //   // new Encoder();
+  // }
+}
