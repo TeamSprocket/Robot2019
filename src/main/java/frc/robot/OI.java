81a5e4d51504624aa122868d2487e25cb3703d79
Catherine <2111catlu@gmail.com>
2019-02-25 16:29:35 -0800

    Add LED lights subsystem

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -33,6 +33,8 @@ public final class OI {
     public static final Button togglePipeline = new JoystickButton(Controllers.rightJoystick, 5);
     public static final Button alignRobot = new JoystickButton(Controllers.leftJoystick, 1);
     public static final Button moveForward = new JoystickButton(Controllers.leftJoystick, 2);
+    
+    public static final Button blinkLED = new JoystickButton(Controllers.leftJoystick, 3);
 
     public static final Button calibrateArm = new JoystickButton(Controllers.gamepad, Y);
 
